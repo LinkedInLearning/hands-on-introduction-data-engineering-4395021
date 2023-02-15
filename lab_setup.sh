4e1ddd25af51b0edc9899e505c690f3c59a222d2
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-02-15 17:21:04 -0500

    fixing typo

--- lab_setup.sh
+++ lab_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sqlite3 manual-etl-database.db <<EOF
-.read create_tables.sql
+.read create_table.sql
 .quit
 EOF
 
 sqlite3 airflow-etl-database.db <<EOF
-.read create_tables.sql
+.read create_table.sql
 .quit
 EOF
