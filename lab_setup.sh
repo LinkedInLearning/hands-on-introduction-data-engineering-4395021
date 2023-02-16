ac9ba2d12f3554f6f87f42d4cfad151046d9ef20
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-02-16 08:50:25 -0500

    updated lab setup

--- lab_setup.sh
+++ lab_setup.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-sqlite3 manual-etl-database.db <<EOF
+mkdir -p /home/codespace/manual
+mkdir -p /home/codespace/orchestrated
+
+sqlite3 /home/codespace/manual/manual-etl-database.db <<EOF
 .read create_table.sql
+.headers on
+.mode csv
 .quit
 EOF
 
-sqlite3 airflow-etl-database.db <<EOF
+sqlite3 /home/codespace/orchestrated/airflow-etl-database.db <<EOF
 .read create_table.sql
+.headers on
+.mode csv
 .quit
 EOF
+
