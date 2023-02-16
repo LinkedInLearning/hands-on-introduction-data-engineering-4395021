d3adfe43a1bcc538a76773512390b422a3522d61
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-02-16 11:04:11 -0500

    changing locations

--- lab_setup.sh
+++ lab_setup.sh
@@ -3,16 +3,17 @@ mkdir -p /home/codespace/manual
 mkdir -p /home/codespace/orchestrated
 
 sqlite3 /home/codespace/manual/manual-etl-database.db <<EOF
-.read create_table.sql
+.read /workspaces/hands-on-introduction-data-engineering-4395021/create_table.sql
 .headers on
 .mode csv
 .quit
 EOF
 
 sqlite3 /home/codespace/orchestrated/airflow-etl-database.db <<EOF
-.read create_table.sql
+.read /workspaces/hands-on-introduction-data-engineering-4395021/create_table.sql
 .headers on
 .mode csv
 .quit
 EOF
+
 
