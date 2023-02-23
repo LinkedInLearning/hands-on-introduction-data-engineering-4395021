05ff1686287c4701ca33145412da8e9f385d3101
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-02-23 16:21:38 -0500

    fixing paths

--- lab_setup.sh
+++ lab_setup.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-# Airflow Home
-export AIRFLOW_HOME="/workspaces/hands-on-introduction-data-engineering-4395021/airflow"
-
 # Paths
-export LAB_BASE_PATH="/workspaces/hands-on-introduction-data-engineering-4395021"
-export LAB_MANUAL_PATH="$BASE_PATH/lab/manual"
-export LAB_ORCHESTRATED_PATH="$BASE_PATH/lab/orchestrated"
-export LAB_ETL_PATH="$BASE_PATH/lab/etl"
-export LAB_AIRFLOW_PATH="$BASE_PATH/airflow"
-export LAB_AIRFLOW_DAGS_PATH="$BASE_PATH/airflow/dags"
+export LAB_BASE_PATH=`pwd`
+export AIRFLOW_HOME="$LAB_BASE_PATH/airflow"
+export LAB_MANUAL_PATH="$LAB_BASE_PATH/lab/manual"
+export LAB_ORCHESTRATED_PATH="$LAB_BASE_PATH/lab/orchestrated"
+export LAB_ETL_PATH="$LAB_BASE_PATH/lab/etl"
+export LAB_AIRFLOW_PATH="$LAB_BASE_PATH/airflow"
+export LAB_AIRFLOW_DAGS_PATH="$LAB_BASE_PATH/airflow/dags"
 
 # Create some of the directories that we'll use
 mkdir -p $LAB_MANUAL_PATH
