39d7644424199159b15ccdc995b53d3361d9569f
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-02-23 22:27:37 -0500

    extract dag completed

---
+++ airflow/dags/extract-dag.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+
+with DAG('extract_dag',
+         schedule_interval=None,
+         start_date=datetime(2022, 1, 1),
+         catchup=False) as dag:
+
+    extract_task = BashOperator(
+        task_id='extract_task',
+        bash_command='wget -c https://datahub.io/core/top-level-domain-names/r/top-level-domain-names.csv.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/airflow-extract-data.csv',
+    )
+
+    extract_task
+
