5959060b4843861cb14c0b0cffd11a51aacde512
Vinoo Ganesh <vinooganesh@users.noreply.github.com>
2023-02-24 15:07:26 +0000

    Updating to exercise end

--- airflow/dags/basic-etl-dag.py
+++ airflow/dags/basic-etl-dag.py
@@ -1,7 +1,8 @@
-from airflow import DAG
-from datetime import datetime, date
-from airflow.operators.python import PythonOperator
 import pandas as pd
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime, date
 
 with DAG(
     dag_id='basic_etl_dag',
@@ -15,7 +16,7 @@ with DAG(
         bash_command='wget -c https://datahub.io/core/top-level-domain-names/r/top-level-domain-names.csv.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/basic-etl-extract-data.csv',
     )
 
-  def transform_data():
+    def transform_data():
       """Read in the file, and write a transformed file out"""
       today = date.today()
       df = pd.read_csv("/workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/basic-etl-extract-data.csv")
@@ -24,19 +25,16 @@ with DAG(
       generic_type_df.to_csv("/workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/basic-etl-transform-data.csv", index=False)
       print(f'Number of rows: {len(generic_type_df)}')
 
-  transform_task = PythonOperator(
+    transform_task = PythonOperator(
        task_id='transform_task',
        python_callable=transform_data,
        dag=dag)
 
-  Transform_task
-
- load_task = BashOperator(
+    load_task = BashOperator(
         task_id='load_task', 
-        bash_command='echo -e ".separator ","\n.import /workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/basic-etl-transform-data.csv top_level_domains" | sqlite3 /workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/basoc-etl-load-db.db',
+        bash_command='echo -e ".separator ","\n.import /workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/basic-etl-transform-data.csv top_level_domains" | sqlite3 /workspaces/hands-on-introduction-data-engineering-4395021/lab/end-to-end/basic-etl-load-db.db',
         dag=dag
     )
-
 
     extract_task >> transform_task >> load_task
 
