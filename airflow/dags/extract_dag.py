5f1993e5f8d495cf04d41a5e0f9b44cd5abce8c5
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-03-01 21:42:38 -0500

    remove pointless statement

--- airflow/dags/extract_dag.py
+++ airflow/dags/extract_dag.py
@@ -12,5 +12,3 @@ with DAG('extract_dag',
             task_id='extract_task',
             bash_command='wget -c https://datahub.io/core/top-level-domain-names/r/top-level-domain-names.csv.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/airflow-extract-data.csv'
             )
-
-    extract_task
