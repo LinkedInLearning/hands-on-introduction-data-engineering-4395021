da1c57c77e82a4d8638c63c7867f525b6069234f
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-03-03 09:19:06 -0500

    add challenge lab_setup

--- lab_setup.sh
+++ lab_setup.sh
@@ -6,7 +6,9 @@ export LAB_MANUAL_PATH="$LAB_BASE_PATH/manual"
 export LAB_ORCHESTRATED_PATH="$LAB_BASE_PATH/orchestrated"
 export LAB_END_TO_END_PATH="$LAB_BASE_PATH/end-to-end"
 export LAB_TEMP_PATH="$LAB_BASE_PATH/temp"
+export LAB_CHALLENGE_PATH="$LAB_BASE_PATH/challenge"
 export LAB_AIRFLOW_PATH="$LAB_BASE_PATH/airflow"
+
 
 # Manual Sqlite Database
 sqlite3 "$LAB_MANUAL_PATH/manual-load-db.db" <<EOF
@@ -25,3 +27,10 @@ sqlite3 "$LAB_END_TO_END_PATH/basic-etl-load-db.db" <<EOF
 .read create_table.sql
 .quit
 EOF
+
+# Challenge Sqlite Database
+sqlite3 "$LAB_CHALLENGE_PATH/challenge-load-db.db" <<EOF
+.quit
+EOF
+
+
