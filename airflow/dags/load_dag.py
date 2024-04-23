e3abb62917927e8bfef57f74e32a07370a32ef94
Eze Anthony Anayo <101370804+Brunchcode@users.noreply.github.com>
2024-04-23 23:00:32 +0000

    completed

---
+++ airflow/dags/load_dag.py
@@ -0,0 +1,15 @@
+''' Load DAG '''
+from datetime import datetime
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+
+with DAG('load_dag',
+    start_date=datetime(2024, 4, 23),
+    schedule_interval=None,
+    catchup=False) as dag:
+
+    load_task = BashOperator(
+        task_id='load_task',
+        bash_command='echo -e ".separator ","\n.import --skip 1 /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/airflow-transform-data.csv top_level_domains" | sqlite3 /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/airflow-load-db.db',
+        dag=dag
+    )
