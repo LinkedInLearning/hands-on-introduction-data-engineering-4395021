287b4e1feba184db1ad02f9ef16efe4d9fcca6b3
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2024-04-24 10:32:57 -0400

    Update location of top-level-domain-names file

--- airflow/dags/extract_dag.py
+++ airflow/dags/extract_dag.py
@@ -7,8 +7,9 @@ with DAG('extract_dag',
          schedule_interval=None,
          start_date=datetime(2022, 1, 1),
          catchup=False) as dag:
-
+        
+    # The location of the top-level-domain-names.csv file has changed. Now, it is hosted in the same repository as this project.
     extract_task = BashOperator(
             task_id='extract_task',
-            bash_command='wget -c https://datahub.io/core/top-level-domain-names/r/top-level-domain-names.csv.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/airflow-extract-data.csv'
+            bash_command='wget -c https://raw.githubusercontent.com/LinkedInLearning/hands-on-introduction-data-engineering-4395021/main/data/top-level-domain-names.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/airflow-extract-data.csv'
             )
